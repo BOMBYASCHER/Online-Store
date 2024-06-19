e2026caac0fd7e16fe2594df3ea0be2c9005dd8e
LoseGameng <losegameng@gmail.com>
2024-06-19 17:14:32 +0300

    fix

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -79,7 +79,7 @@ const addCartToHTML = () => {
                 <div class="name">
                 ${info.name}
                 </div>
-                <div class="totalPrice">$${info.price * item.quantity}</div>
+                <div class="totalPrice">₽${info.price * item.quantity}</div>
                 <div class="quantity">
                     <span class="minus"><</span>
                     <span>${item.quantity}</span>
